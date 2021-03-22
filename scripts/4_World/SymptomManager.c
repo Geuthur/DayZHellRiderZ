82b7c1b992b23aa73e6eb766af898f0f8f12aa36
Geuthur <Netrom@chello.at>
2021-03-22 16:51:46 +0100

    Update RegisterSymptom - HRZ_KokainSymptom

--- scripts/4_World/SymptomManager.c
+++ scripts/4_World/SymptomManager.c
@@ -1,11 +1,11 @@
-enum SymptomIDs2 {
-	SYMPTOM_SHAKEHRZ = 1099
+enum HRZ_SymptomIDs {
+	SYMPTOM_SHAKEHRZ = 1001
 };
 modded class SymptomManager
 {
 	override void Init()
 	{
 		super.Init();
-		RegisterSymptom(new HRZ_ShakeSymptom);
+		RegisterSymptom(new HRZ_KokainSymptom);
 	}
 }
