3c82a7ca0ea4c7f16f0440123db79ca22f49197d
Geuthur <Netrom@chello.at>
2021-03-24 23:51:38 +0100

    Vehicle Get In Log Fix

--- scripts/4_World/PermissionSystem/AdminLog/VehicleLog.c
+++ scripts/4_World/PermissionSystem/AdminLog/VehicleLog.c
@@ -21,3 +21,41 @@ modded class ActionGetOutTransport: ActionInteractBase
         m_VehicleAdminLog.HRZ_ActionGetOutTransport(action_data.m_Player, HRZ_Transport);
    }
 }
+
+modded class ActionGetInTransport: ActionInteractBase
+{
+    HRZ_PluginAdminLog m_VehicleAdminLog;
+    Transport HRZ_Transport; 
+
+    override bool ActionCondition( PlayerBase player, ActionTarget target, ItemBase item )
+    {
+        HRZ_Transport = null;
+        Class.CastTo(HRZ_Transport, target.GetObject());
+
+        if (!super.ActionCondition(player, target, item))        
+            return false;        
+
+		ItemBase holdItem = player.GetItemInHands();
+		
+		if (holdItem) 
+        {
+            if (holdItem.m_ItemBehaviour == 0 || HRZ_Transport == null)
+                return false;
+
+            int crew_index = HRZ_Transport.CrewPositionIndex(target.GetComponentIndex());
+
+            if (crew_index == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    override void OnEndServer(ActionData action_data) 
+    {
+        super.OnEndServer(action_data);
+
+        m_VehicleAdminLog = HRZ_PluginAdminLog.Cast( GetPlugin(HRZ_PluginAdminLog) );
+        m_VehicleAdminLog.HRZ_ActionGetInTransport(action_data.m_Player, HRZ_Transport);
+   }
+}
