5721f755534989a0a865b6b49843ce5fb8a966d8
Geuthur <Netrom@chello.at>
2021-03-27 20:22:26 +0100

    Update Effect Crystal

--- scripts/4_World/SymptomSystem/Symptoms/CrystalSymptom.c
+++ scripts/4_World/SymptomSystem/Symptoms/CrystalSymptom.c
@@ -1,7 +1,6 @@
 class HRZ_CrystalSymptom extends SymptomBase
  {
-	
-  //this is just for the Symptom parameters set-up and is called even if the Symptom doesn't execute, don't put any gameplay code in here
+	//this is just for the Symptom parameters set-up and is called even if the Symptom doesn't execute, don't put any gameplay code in here
   override void OnInit()
   {
 	m_SymptomType = SymptomTypes.SECONDARY;
@@ -16,27 +15,33 @@ class HRZ_CrystalSymptom extends SymptomBase
   {
   }
   
+  private 	  float 					m_PulseTimer;
   override void OnUpdateClient(PlayerBase player, float deltatime)
   {
+		m_PulseTimer += deltatime;
+		int m_cycle = m_PulseTimer;
+		if (m_cycle > 5 && m_cycle < 11){
+		HRZ_PPEffects.PulsingRadialEffect(HRZ_EffectID.Crystal, 1);
+		} else {
+		HRZ_PPEffects.SetRadialBlurEffectValue(HRZ_EffectID.Crystal,0, 0, 0.76, 0);
+		}
   }
   
   override void OnGetActivatedServer(PlayerBase player)
   {
   }
-  
+	
   override void OnGetActivatedClient(PlayerBase player)
   {
         super.OnGetActivatedClient(player);
 
         Event_OnActivatedClient.Invoke(player, Type()); // pass player pointer and typename
-	
-		PPEffects.SetVignetteEffectValue(2, 3, 250, 127, 4, 0.2);
-		PPEffects.SetBloodSaturation(5);
 		
-		HRZ_PPEffects.SetChromAbers(0.1, 0.1);
-		HRZ_PPEffects.SetCrystalDynamic(20.0);
-		HRZ_PPEffects.SetCrystalRotiBlur(0.50, 2.50, 10.0);
-		HRZ_PPEffects.SetCrystalRadialBlur(0, 0, 0.76, 0);
+		HRZ_PPEffects.SetChromAbersEffectValue(HRZ_EffectID.Crystal,0.1, 0.1);
+		HRZ_PPEffects.HRZ_SetVignetteEffectValue(HRZ_EffectID.Crystal, 3, 250, 127, 4, 0.2);
+		PPEffects.SetBloodSaturation(5);
+		HRZ_PPEffects.SetDynamicEffectValue(HRZ_EffectID.Crystal,20.0);
+		HRZ_PPEffects.SetRotiBlurEffectValue(HRZ_EffectID.Crystal,0.50, 2.50, 10.0);
   }
   
   override void OnGetDeactivatedServer(PlayerBase player)
@@ -50,10 +55,8 @@ class HRZ_CrystalSymptom extends SymptomBase
 
         Event_OnDeactivatedClient.Invoke(player, Type()); // pass player pointer and typename
 
-		PPEffects.ResetVignettes();
-		PPEffects.SetBloodSaturation(1);
-
 		HRZ_PPEffects.ResetAll();
+		PPEffects.ResetAll();
 		
   Debug.Log("OnGetDeactivated ShakeSymptom called", "PlayerSymptom");
   }
